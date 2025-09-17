4952128d542bd18ef35fdd1aad6680230ebba281
Graham Monkman <gmonkman@mistymountains.biz>
2025-09-17 10:35:57 +0100

    work

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ DB_PATH = 'C:/development/price_watch/prices.db'
 
 # Where a single monitor/parser checks across multiple pages (e.g. CCL 9070xt),
 # what is the mean delay between pages.
-SCAPE_DELAY_BETWEEN_PAGES_SECONDS = 5
+SCRAPE_DELAY_BETWEEN_PAGES_SECONDS = 5
 SCRAPE_DELAY_RANDOM_FACTOR = 0.2  # i.e. 20%, so 5 seconds would be randomised between 4 and 6 seconds
 
 # Notifiers to use, as enums
